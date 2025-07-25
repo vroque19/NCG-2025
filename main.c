e413764d2d2c57134db6b689f964bd004ed6c303
Vanessa Roque <viroque19@gmail.com>
2025-07-25 15:28:28 -0700

    corrected channel0 and got ADC readings

--- main.c
+++ main.c
@@ -9,11 +9,6 @@
 #include "led.h"
 #include "spi.h"
 #include "include/4131.h"
-extern uint8_t main_rx[TX_DATA_LEN];
-// uint8_t main_rx[TX_DATA_LEN];
-// void ADC_SPI_Init(void);
-void spi_main_init(void);
-
 
 void spi_main_init(void)
 {
@@ -27,28 +22,14 @@ void spi_main_init(void)
     }
     // SET DATA SIZE
     MXC_SPI_SetDataSize(SPI_MAIN, WORD_SIZE);
-    // SET MDOE
+    // SET MODE
     MXC_SPI_SetMode(MXC_SPI1, SPI_MODE_3);
+    // SET WIDTH
+    MXC_SPI_SetWidth(SPI_MAIN, SPI_WIDTH_STANDARD);
     // SET GPIO FOR SPI
     const mxc_gpio_cfg_t gpio_cfg_spi1_vddioh = { MXC_GPIO1, (MXC_GPIO_PIN_26 | MXC_GPIO_PIN_28 | MXC_GPIO_PIN_23 |MXC_GPIO_PIN_29),
         MXC_GPIO_FUNC_ALT1, MXC_GPIO_PAD_NONE, MXC_GPIO_VSSEL_VDDIOH, MXC_GPIO_DRVSTR_0 };
-    // SET CS PIN
-    mxc_gpio_cfg_t cs_pin_config = {
-        .port = CS_PORT,
-        .mask = CS_PIN,
-        .pad = MXC_GPIO_PAD_NONE,
-        .func = MXC_GPIO_FUNC_ALT1,
-        .vssel = MXC_GPIO_VSSEL_VDDIOH, // Set voltage to 3.3V
-        .drvstr = MXC_GPIO_DRVSTR_0     // Default drive strength
-    };
-    // CHECK FOR ERRORS
-    if (MXC_GPIO_Config(&cs_pin_config) != E_NO_ERROR)
-    {
-        printf("\nCS PIN CONFIGURATION ERROR\n");
-        while (1)
-        {
-        }
-    }
+
     if (MXC_GPIO_Config(&gpio_cfg_spi1_vddioh) != E_NO_ERROR)
     {
         printf("\nCS PIN CONFIGURATION ERROR\n");
@@ -56,8 +37,6 @@ void spi_main_init(void)
         {
         }
     }
-    // DEASSERT CS (PULL HIGH)
-    //MXC_GPIO_OutSet(CS_PORT, CS_PIN);
     printf("SPI Master Initialization Complete. Speed: %d Hz\n", SPI_SPEED);
 }
 
@@ -68,6 +47,7 @@ void spi_main_init(void)
 
 void test_spi_send(void)
 {
+    uint8_t main_rx[TX_DATA_LEN];
     uint8_t tx_buff[] = {0x02, 0x03, 0x04};
     printf("Sending %d bytes\n", TX_DATA_LEN);
     printf("\n");
@@ -84,10 +64,13 @@ void test_spi_send(void)
 
 int main(void)
 {
-    // SPI transaction request
     spi_main_init();
+    read_adc_conversion();
     write_mem_map();
-
+    // read_status();
+    read_adc_id();
+    read_adc_conversion();
+    // read_status();
     MXC_SPI_Shutdown(SPI_MAIN);
     return E_NO_ERROR;
 }
