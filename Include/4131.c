fdd9f822c5d9db26890e30673e181dd4c67d5535
Vanessa Roque <viroque19@gmail.com>
2025-07-27 19:16:44 -0700

    fix print statements

--- Include/4131.c
+++ Include/4131.c
@@ -13,12 +13,11 @@ uint8_t main_rx[TX_DATA_LEN];
 
   
 void print_buff(const uint8_t *buff, size_t len) {
-    printf("Sending ");
+    printf("Sending %d bytes:", len);
     for(int i = 0; i < len; i++) {
         printf(" 0x%2X ", buff[i]);
         // MXC_Delay(MXC_DELAY_USEC(100));
     }
-    printf("\n");
     printf("\n");
 }
 
@@ -26,7 +25,6 @@ void print_buff_received(const uint8_t *buff, size_t len) {
     printf("Received ");
     for(int i = 0; i < len; i++) {
         printf(" 0x%2X ", buff[i]);
-        // MXC_Delay(MXC_DELAY_USEC(100));
     }
     printf("\n");
     printf("\n");
@@ -36,7 +34,7 @@ int spi_send_data(const uint8_t *tx_data, const uint8_t *rx_data, size_t len)
 {
    MXC_GPIO_OutClr(CS_PORT, CS_PIN); // ASSERT Chip Select (CS)
     
-    print_buff(tx_data, len);
+    // print_buff(tx_data, len);
     // INITIALIZE MAIN REQUEST PARAMETERS
     mxc_spi_req_t main_req;
     main_req.spi = SPI_MAIN;
@@ -56,7 +54,7 @@ int spi_send_data(const uint8_t *tx_data, const uint8_t *rx_data, size_t len)
     {
         printf("Error in transaction %d\n", res);
     }
-    print_buff_received(rx_data, len);
+    // print_buff_received(rx_data, len);
     return res;
 }
 
@@ -104,7 +102,7 @@ void read_reg(uint8_t reg_addr, size_t bytes) {
     // memcpy(data, &rx_buff[1], bytes - 1);
     // print_buff_received(rx_buff, bytes-1);
     printf("Reading register 0x%2X \n", reg_addr);
-    print_buff_received(&rx_buff[1], bytes-1);
+    print_buff_received(rx_buff, bytes-1);
 }
 
 void set_misc(void) {
@@ -115,9 +113,9 @@ void set_misc(void) {
 
 void set_gain_n(void) {
     size_t bytes = 2;
-    int n = 8;
+    int n = 7;
     uint8_t tx_data[] = {0x55, 0xC6};
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i <= n; i++) {
         set_reg(ADC_GAIN_X(i), tx_data, bytes);
     }
 }
@@ -125,8 +123,8 @@ void set_gain_n(void) {
 void set_offset_n(void) {
     size_t bytes = 2;
     uint8_t tx_data[] = {0x80, 0x00};
-    int n = 2;
-    for(int i = 0; i < n; i++) {
+    int n = 7;
+    for(int i = 0; i <= n; i++) {
         set_reg(ADC_OFFSET_X(i), tx_data, bytes);
     }
 }
@@ -134,8 +132,8 @@ void set_offset_n(void) {
 void set_channel_m(void) {
     size_t bytes = 3;
     uint8_t tx_data[] = {0x00, 0x01, 0x00};
-    int m = 9;
-    for(int i = 1; i < m; i++) {
+    int m = 15;
+    for(int i = 1; i <= m; i++) {
         set_reg(ADC_CHANNEL_X(i), tx_data, bytes);
     }
     return;
@@ -143,9 +141,9 @@ void set_channel_m(void) {
 
 void set_config_n(void) {
     size_t bytes = 2;
-    int n = 2;
-    uint8_t tx_data[] = {0x0, 0x0C};
-    for(int i = 0; i < n; i++) {
+    int n = 7;
+    uint8_t tx_data[] = {0x0, 0x00};
+    for(int i = 0; i <= n; i++) {
         if(i > 1) {
             tx_data[1] = 0x0;
         }
@@ -155,9 +153,9 @@ void set_config_n(void) {
 
 void set_filter_n(void) {
     size_t bytes = 3;
-    int n = 8;
+    int n = 7;
     uint8_t tx_data[] = {0x0, 0x20, 0xA0};
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i <= n; i++) {
         if (i > 1) {
             tx_data[2] = 0x30;
         }
@@ -167,7 +165,7 @@ void set_filter_n(void) {
 
 void set_channel_0(void) {
     size_t bytes = 3;
-    uint8_t tx_data[] = {0x08, 0x01, 0x00};
+    uint8_t tx_data[] = {0x80, 0x01, 0x00};
     set_reg(ADC_CHANNEL_X(0), tx_data, bytes);
 }
 
@@ -208,8 +206,8 @@ void set_mclk_count(void) {
 void write_mem_map(void) {
     // write to 4131 registers to configure for AIN0-AIN5
     // set_status();
-    set_ctrl();
-    set_data();
+    // set_ctrl();
+    // set_data();
     set_io_ctrl();
     set_vbias_ctrl();
     // set_mclk_count();
@@ -217,16 +215,23 @@ void write_mem_map(void) {
     set_channel_m();
     set_config_n();
     set_filter_n();
+    set_offset_n();
     set_gain_n();
     set_misc();
+    printf("\n >>> Successfully set the 4131 registers << \n\n");
 }
 
 void read_adc_id(void) {
     size_t bytes = 2;
-    read_reg(0x05, bytes);
-    // uint8_t tx_buff[] = {0x85, 0x00, 0x00};
-
-    // spi_send_data(tx_buff, main_rx, bytes);
+    read_reg(ADC_ID, bytes);
 }
 
+void read_adc_conversion(void) {
+    size_t bytes = 3;
+    read_reg(ADC_DATA, bytes);
+}
 
+void read_status(void) {
+    size_t bytes = 2;
+    read_reg(ADC_STATUS, bytes);
+}
