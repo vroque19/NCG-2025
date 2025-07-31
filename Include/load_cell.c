e77d25a580626869a6ab04881d13a67e8bb61c4b
Vanessa Roque <viroque19@gmail.com>
2025-07-31 15:02:41 -0700

    add functions for load cells 1 and 2

--- Include/load_cell.c
+++ Include/load_cell.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
 #include "mxc_delay.h"
@@ -8,36 +9,70 @@
 #include "4131.h"
 #include "load_cell.h"
 
+// calibrate the scale
 // converts ADC code to weight
 
-double get_average(uint32_t *list, uint8_t n) {
-    // get the length of the list
+uint32_t get_average(uint32_t *list, uint8_t n) {
     uint32_t sum = 0;
     for(int i = 0; i < n; i++) {
-        printf("%d: ", i);
-        printf("%d\n", list[i]);
         sum += list[i];
     }
-    double res = sum / n;
+    uint32_t res = floor(sum / n);
     printf("\n\n");
-    // printf("resulting avg: %f", res);
     return res;
 }
 
 
-void get_calibration_data(void) {
+uint32_t get_calibration_data(void) {
     int n = 3;
     uint32_t data[n];
     for(int i = 0; i < n; i++) {
         data[i] = get_adc_data();
-        printf(">>>> code: %d\n", data[i]);
+        // printf(">>>> code: %d\n", data[i]);
         MXC_Delay(MXC_DELAY_MSEC(10));
 
-
     }
-    printf("~~Calibrated~~\n");
-    double res = get_average(data, n);
-    printf("resulting avg: %.5f\n", res);
+    uint32_t res = get_average(data, n);
+    // printf("resulting avg: %d\n\n", res);
+    return res;
+}
+
+uint32_t calibrate(void) {
+    printf("~~Taring the scale~~\n");
+    uint32_t base = get_calibration_data();
+    printf("0.00~=code: %d\n\n", base);
+    return base;
+}
+
+double code_to_grams(uint32_t base, uint32_t code, double conversion_factor){
+    printf("%d - %d\n", code, base);
+    double delta = abs((int)code - (int)base);
+    if(delta > 2) {
+        double grams = delta * conversion_factor;
+        return grams;
+    }
+    // printf("Weight in grams: %dg\n\n", grams);
+    return 0;
+}
+
+
+void load_cell_1_read(uint32_t base) {
+    printf("0.00~=code: %d\n\n", base);
+    uint32_t code = get_adc_data();
+    double conversion_factor = 0.766561084;
+    double weight = code_to_grams(base, code, conversion_factor);
+
+    printf("Weight in grams: %.2fg\n\n", weight);
+
+}
+void load_cell_2_read(uint32_t base) {
+    printf("0.00~=code: %d\n\n", base);
+    uint32_t code = get_adc_data();
+    double conversion_factor = 0.7532382584;
+    double weight = code_to_grams(base, code, conversion_factor);
+    printf("Weight in grams: %.2fg\n\n", weight);
+
+
 }
 
 
--- Include/load_cell.h
+++ Include/load_cell.h
@@ -1,8 +1,12 @@
 #ifndef _load_cell_h_
 #define _load_cell_h_
 
+uint32_t calibrate(void);
 void test_get_average(void);
-void get_calibration_data(void);
-double get_average(uint32_t *list, uint8_t n);
-
+uint32_t get_calibration_data(void);
+uint32_t get_average(uint32_t *list, uint8_t n);
+void load_cell_1_read(uint32_t base);
+void load_cell_2_read(uint32_t base);
+double get_weight(uint32_t base);
+double code_to_grams(uint32_t base, uint32_t code, double conversion_factor);
 #endif
