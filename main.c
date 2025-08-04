794fd56add8932718199bc5a810cc03fc43c6a09
Vanessa Roque <viroque19@gmail.com>
2025-08-04 11:00:20 -0700

    2 workings load cells

--- main.c
+++ main.c
@@ -18,18 +18,34 @@ int main(void)
     spi_main_init();
     write_mem_map();
     read_adc_id();
-    uint32_t base1 = calibrate();
-    MXC_Delay(MXC_DELAY_MSEC(1000));
-    set_channel_2();
-    uint32_t base2 = calibrate();
-    MXC_Delay(MXC_DELAY_MSEC(1000));
+    read_adc_conversion();
+    // set_channel_1();
+    // set_channel_1();
+    // set_channel_1();
+    // set_channel_0();
+    // set_channel_1();
+    
+    // set_channel_0();
+    // set_channel_1();
+    // set_channel_0();
+    uint32_t base0 = calibrate(0);
+    MXC_Delay(MXC_DELAY_MSEC(1500));
+
+    set_channel_1();
+    // read_status();
+    // set_channel_1();
+    uint32_t base1 = calibrate(1);
+    // read_status();
+
     while(1) {
-        set_channel_0();
-        load_cell_1_read(base1);
-        MXC_Delay(MXC_DELAY_MSEC(1000));
-        set_channel_2();
-        load_cell_2_read(base2);
-        MXC_Delay(MXC_DELAY_MSEC(1000));
+        // get_load_cell_data(0, base0);
+        // get_data_from_channel(0);
+        // MXC_Delay(MXC_DELAY_MSEC(5));
+        get_load_cell_data(0, base0);
+        // load_cell_1_read(base1);
+        MXC_Delay(MXC_DELAY_MSEC(500));
+        get_load_cell_data(1, base1);
+        MXC_Delay(MXC_DELAY_MSEC(500));
     }
     MXC_SPI_Shutdown(SPI_MAIN);
     return E_NO_ERROR;
