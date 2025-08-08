1359e2cf81f2cba8cb3f3a22f01e36b31da364d9
Vanessa Roque <viroque@gmail.com>
2025-08-08 15:43:03 -0700

    add api for changing weight text

--- Include/nextion.c
+++ Include/nextion.c
@@ -18,37 +18,37 @@ void nextion_init(void) {
     };
     // Some parameters are set to defaults as follows: UART Data Size - 8 bits UART Stop Bits - 1 bit UART Parity - None UART Flow Control - None UART Clock - 7.37MHz Clock (for baud > 7372800, PCLK is used)
 
-    if (MXC_GPIO_Config(&uart_gpio) != E_NO_ERROR)
-    {
+    if (MXC_GPIO_Config(&uart_gpio) != E_NO_ERROR) {
         printf("\nCS PIN CONFIGURATION ERROR\n");
-        while (1)
-        {
-        }
+        while (1){}
     }
     printf("\nUART Baud \t: %d Hz\n", BAUD_RATE);
-    // printf("Baud0: %d, Baud1%d\n", NEXTION_UART_REG->baud0, NEXTION_UART_REG->baud1);
-    // MXC_GPIO_Config(&uart_gpio);
     printf("\nSuccesfull Initialized UART\n\n");
 }
 
-
 void nextion_send_command(const char *command) {
     // Send the command string byte by byte.
-    char prefix[] = "t0.txt=";
-    for (int i = 0; i < strlen(prefix); ++i) {
-        MXC_UART_WriteCharacter(NEXTION_UART_REG, prefix[i]);
-    }
     for (int i = 0; i < strlen(command); ++i) {
         MXC_UART_WriteCharacter(NEXTION_UART_REG, command[i]);
     }
-    terminate_command();
-    // NEXTION commands must be terminated with three 0xFF bytes.
+    terminate_command(); // commands must be terminated with three 0xFF bytes.
 }
 
 void terminate_command(void) {
+    // 3 0xFF bytes
     for (int i = 0; i < 3; ++i) {
         MXC_UART_WriteCharacter(NEXTION_UART_REG, 0xFF);
     }
     MXC_Delay(MXC_DELAY_MSEC(50));
+    printf("Sent\n");
 }
 
+// update the weight output text on the display
+void update_weight(float weight, char *objname) {
+    char prefix[] = ".txt=\"";
+    char dest_buff[50]; // final command
+    char suffix[] = "\"";
+    snprintf(dest_buff, sizeof(dest_buff), "%s%s%.2f%s", objname, prefix, weight, suffix); // combine obj, pref, weight, suff into one commands
+    printf("Combined command: %s\n", dest_buff); // debugging what is being sent
+    nextion_send_command(dest_buff);
+}
--- Include/nextion.h
+++ Include/nextion.h
@@ -13,14 +13,14 @@
 #define UART_PORT     MXC_GPIO_PORT_2
 #define TX_PIN         MXC_GPIO_PIN_16
 #define RX_PIN     MXC_GPIO_PIN_14
-
+#define TXT_WEIGHT_0 "t0"
+#define TXT_WEIGHT_1 "t1"
+#define TXT_WEIGHT_2 "t2"
 void nextion_init(void);
-void send_test(void);
-void nextion_send_command(const char *command);
-void nextion_send_hex(const char* command);
-int send_req(void);
-void print_buff(const uint8_t *buff, size_t len);
-void send_read_test(void);
+void nextion_send_command(const char *command); 
+// "n0.val=4343" or label0.txt="jfjjf"
 void terminate_command(void);
+void update_weight(float weight, char *objname);
+
 
 #endif
