57f14e54fe68b2241c026a363a42a0c5550fad0d
Vanessa Roque <viroque19@gmail.com>
2025-08-11 23:28:42 -0700

    wip, convert to using include dir

--- main.c
+++ main.c
@@ -1,51 +1,49 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <string.h>
-#include <stddef.h>
-#include <math.h>
-#include "mxc_device.h"
-#include "mxc_delay.h"
-#include "mxc_pins.h"
-#include "uart.h"
+#include "Include/4131.h"
+#include "Include/load_cell.h"
 #include "led.h"
+#include "mxc_delay.h"
+#include "mxc_device.h"
+#include "mxc_pins.h"
 #include "spi.h"
-#include "include/4131.h"
-#include "include/load_cell.h"
+#include "uart.h"
+#include <math.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
+int main(void) {
+  spi_main_init();
+  write_mem_map();
+  read_adc_id();
 
-int main(void)
-{
-    spi_main_init();
-    write_mem_map();
-    read_adc_id();
+  uint32_t base0 = calibrate(0);
+  MXC_Delay(MXC_DELAY_MSEC(1500));
 
-    uint32_t base0 = calibrate(0);
-    MXC_Delay(MXC_DELAY_MSEC(1500));
-    
-    uint32_t base1 = calibrate(1);
-    MXC_Delay(MXC_DELAY_MSEC(1500));
-    
-    uint32_t base2 = calibrate(2);
-    while(1) {
-        printf("Select a load cell 0-2:\n ");
-        uint8_t cell_idx;
-        scanf("%d", &cell_idx);
-        printf("%d\n", cell_idx);
-        
-        switch(cell_idx) {
-            case 0:
-                get_load_cell_data(cell_idx, base0);
-                break;
-            case 1:
-                get_load_cell_data(cell_idx, base1);
-                break;
-            default:
-                get_load_cell_data(cell_idx, base2);
-                break;
-        }
-        // get_load_cell_data(2, base2);
-        MXC_Delay(MXC_DELAY_MSEC(500));
+  uint32_t base1 = calibrate(1);
+  MXC_Delay(MXC_DELAY_MSEC(1500));
+
+  uint32_t base2 = calibrate(2);
+  while (1) {
+    printf("Select a load cell 0-2:\n ");
+    uint8_t cell_idx;
+    scanf("%d", &cell_idx);
+    printf("%d\n", cell_idx);
+
+    switch (cell_idx) {
+    case 0:
+      get_load_cell_data(cell_idx, base0);
+      break;
+    case 1:
+      get_load_cell_data(cell_idx, base1);
+      break;
+    default:
+      get_load_cell_data(cell_idx, base2);
+      break;
     }
-    MXC_SPI_Shutdown(SPI_MAIN);
-    return E_NO_ERROR;
+    // get_load_cell_data(2, base2);
+    MXC_Delay(MXC_DELAY_MSEC(500));
+  }
+  MXC_SPI_Shutdown(SPI_MAIN);
+  return E_NO_ERROR;
 }
