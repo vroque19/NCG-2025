659cb1e364f8d852e6ac54f13d866721b4585515
Vanessa Roque <viroque19@gmail.com>
2025-09-04 10:21:09 -0700

    change automated id and flashed new display design

--- include/moves.h
+++ include/moves.h
@@ -4,6 +4,7 @@
 #include "stack.h"
 #include "history.h"
 #include "game_logic.h"
+#include <stdint.h>
 
 /**
  * @brief Handles movement from source tower to destination
@@ -13,6 +14,9 @@
  */
 void hanoi_execute_move(uint8_t source_tower, uint8_t destination_tower);
 
+void grab_ring(int ring_height);
+
+void move_to_tower(uint8_t tower_idx);
 /**
  * TODO:
  * 1. Add moves (go to tower x, pick up ring, etc.) that call the TMC driver
