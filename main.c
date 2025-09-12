f3f0014bb43466a194027b5a9fbf6e63fc43e091
Alexander Goldstein <Alexander.Goldstein@analog.com>
2025-09-12 15:11:22 -0700

    Revert "Remove application code for sg2 prototyping"
    This reverts commit 4afc8092a9e58c66288de7b4c01d55c06e304219.

--- main.c
+++ main.c
@@ -128,13 +128,37 @@ int main(void)
 
 	tmc5272_rotateByMicrosteps(tmc_x, MOTOR_0, 50000);
 
+	// Require user pushbutton for Tricoder start
+	printf("Press PB to start Tricoder operation. \n");
+	while(!PB_IsPressedAny()) {}
+	MXC_Delay(MXC_DELAY_MSEC(500));
 
 	// Main Loop
 
     while (1) {
+		// Read the Tricoder position
+		int32_t tc_x_pos = tmc5272_tricoder_getPosition(tmc_tc, TC_X);
+		int32_t tc_y_pos = tmc5272_tricoder_getPosition(tmc_tc, TC_Y);
 
-		
-		
+		// Rotate each axis to its encoder position
+		tmc5272_rotateToPosition(tmc_x, MOTOR_0, 10*tc_x_pos);
+		tmc5272_rotateToPosition(tmc_y, ALL_MOTORS, 10*tc_y_pos);
+
+		// Readout position & encoder
+		printf("Mx0: %d  ENC: %d", tmc5272_getPosition(tmc_x, MOTOR_0), tc_x_pos);
+		printf("\tMy0: %d  ENC: %d \n", tmc5272_getPosition(tmc_y, MOTOR_0), tc_y_pos);
+
+		// Failsafe brake
+		if(PB_IsPressedAny())
+		{
+			printf("Failsafe! \n");
+			tmc5272_rotateAtVelocity(tmc_x, MOTOR_0, 0, 50000);
+			tmc5272_rotateAtVelocity(tmc_x, MOTOR_1, 0, 50000);
+			tmc5272_rotateAtVelocity(tmc_y, MOTOR_0, 0, 50000);
+			tmc5272_rotateAtVelocity(tmc_y, MOTOR_1, 0, 50000);
+			
+			while(1) {}
+		}
 		
 
     }
